d7b40f027cdfd3f08e56f09d7d2a04b1d4e7b044
Matteadd <45122381+Matteadd@users.noreply.github.com>
2020-03-18 15:09:09 +0100

    ricreato grafico e introfuzione thread

--- Main.py
+++ Main.py
@@ -1,3 +1,5 @@
+import time
+
 from matplotlib.backends.backend_tkagg import FigureCanvasTkAgg
 from matplotlib.figure import Figure
 from mpl_toolkits.mplot3d import Axes3D
@@ -15,134 +17,75 @@ except ImportError:
     import tkinter.ttk as ttk
     py3 = True
 
+try:
+    from mpu6050 import mpu6050
+except ImportError:
+    mpu6050 = None
+
+mpu = mpu6050(0x68) if mpu6050 is not None else ""
+
+import random
+
 
 class Gui():
 
     def __init__(self, window):
 
-        # def unzipImgProgetto():
-        #     from zipfile import is_zipfile, ZipFile
-        #     srcImg = []
-        #     if is_zipfile(self.pathZipProgetto):
-        #         archiveMembers = ZipFile(self.pathZipProgetto).namelist()
-        #         for archiveMember in archiveMembers:
-        #             if ".jpg" in str(archiveMember) or ".png" in str(archiveMember):
-        #                 fileUnzipped = ZipFile(self.pathZipProgetto).open(archiveMember)
-        #                 srcImg.append(fileUnzipped)
-        #     return srcImg
-        #
-        # labelFromFile = ChirurgiaGuidataOsProperties.ChirurgiaGuidataOsProperties.getLabelViews()["Plotter"]
-        #
-        # valueLabelTemp = tk.StringVar()
-        # valueLabelTemp.set(f"{labelFromFile['labeltitolotemperatura']} 0.0 C°")
-        #
-        # valueTimer = tk.StringVar()
-        # valueTimer.set(f"{labelFromFile['labeltitolotimer']} 00:00:00")
-        #
-        # valueLabelAlert = tk.StringVar()
-        # valueLabelAlert.set(f"Status: Ok")
-        #
-        # valueProgressX = tk.IntVar()
-        # valueProgressX.set(0)
-        #
-        # valueProgressY = tk.IntVar()
-        # valueProgressY.set(0)
-        #
-        # valueProgressZ = tk.IntVar()
-        # valueProgressZ.set(0)
-        #
+        self.play = False
+        self.dataX = [i for i in range(0, 50, )]
+        self.dataY = [[0] * 50, [0] * 50, [0] * 50]
+
         topLevel = root
-        # topLevel.transient(self.top)
-        # topLevel.grab_set()
-        #
         topLevel.title("Chirurgia Guidata OS")
         topLevel.geometry(f"1000x1000+0+0")
-        # topLevel.resizable(0, 0)
-        # topLevel.protocol("WM_DELETE_WINDOW", lambda: self.closeTopLevelPlotter(topLevel))
-        # topLevel.configure(bg="#ffffff")
-        #
-        # headerFrame = ttk.Frame(topLevel)
-        # headerFrame.place(relx=0.0, rely=0.0, height=90, relwidth=1.0)
-        # headerFrame.configure(relief='flat')
-        # headerFrame.configure(borderwidth="2")
-        #
-        # imgTitoloLabel = ttk.Label(headerFrame)
-        # imgTitoloLabel.place(relx=0.15, rely=0.05, height=70, width=505)
-        # imgTitoloLabel.configure(relief="flat")
-        # testoHeader = GraphicUtility.ridimensionaImage("img/TestoHeader.png", GraphicUtility.getWidth(imgTitoloLabel),
-        #                                                GraphicUtility.getHeight(imgTitoloLabel))
-        # imgTitoloLabel.configure(image=testoHeader)
-        # imgTitoloLabel.image = testoHeader
-        #
-        # logoLabel = ttk.Label(headerFrame)
-        # logoLabel.place(relx=0.01, rely=0.0, height=70, width=150)
-        # logoLabel.configure(relief="flat")
-        # logo = GraphicUtility.ridimensionaImage("img/Logo.png", GraphicUtility.getWidth(logoLabel),
-        #                                         GraphicUtility.getHeight(logoLabel))
-        # logoLabel.configure(image=logo)
-        # logoLabel.image = logo
-        #
-        fig = Figure(facecolor="white", frameon=False, constrained_layout=True, linewidth=2)
+        topLevel.configure(bg="#ffffff")
+
+        accLegends = ['Acc_x', 'Acc_y', 'Acc_z']
+        gyroLegends = ['Gyro_x', 'Gyro_y', 'Gyro_z']
+
+        fig = Figure(facecolor="red", frameon=False, constrained_layout=True)
+        grafico2d = FigureCanvasTkAgg(fig, topLevel)
         accAxes = fig.add_subplot(211)
         accAxes.set_xticklabels([])
         accAxes.set_xlim(50, 0)
         accAxes.set_ylim([-30, 30])
-        #
+
         gyroAxes = fig.add_subplot(212)
         gyroAxes.set_xticklabels([])
         gyroAxes.set_xlim(50, 0)
         gyroAxes.set_ylim([-300, 300])
 
-        accLegends = ['Acc_x', 'Acc_y', 'Acc_z']
-        gyroLegends = ['Gyro_x', 'Gyro_y', 'Gyro_z']
-
-        timeValues = [i for i in range(0, 50, )]
-        accValues = [[0] * 50, [0] * 50, [0] * 50]
-        gyroValues = [[0] * 50, [0] * 50, [0] * 50]
-
-        # fig3d = Figure(facecolor="white", frameon=True, constrained_layout=True, linewidth=10, edgecolor="#04253a")
-        fig3d = Figure()
+        fig3d = Figure(facecolor="red", frameon=True, constrained_layout=True)
+        fig3d.set_constrained_layout_pads(w_pad=0, h_pad=0, wspace=0, hspace=0)
         grafico3d = FigureCanvasTkAgg(fig3d, topLevel)
-        grafico3d.draw()
-        ax3d = fig3d.add_subplot(111, projection="3d")
+        ax3d = fig3d.add_subplot(111, projection="3d", elev=45, azim=45)
         ax3d.set_xticklabels([])
         ax3d.set_yticklabels([])
         ax3d.set_zticklabels([])
-
-        # plt.cla()
         ax3d.set_xlim(-2, 2)
         ax3d.set_ylim(-2, 2)
         ax3d.set_zlim(-2, 2)
-        ax3d.plot([0, 0], [0, 0], [0, 0], color="blue", marker="o", markevery=[-1])
-        ax3d.plot([0, 0], [0, 0], [0, 0], color="red", marker="o", markevery=[-1])
+        # ax3d.plot([0, 0], [0, 0], [0, 0], color="blue", marker="o", markevery=[-1])
+        # ax3d.plot([0, 0], [0, 0], [0, 0], color="red", marker="o", markevery=[-1])
         ax3d.plot([0, 0], [0, 0], [0, 0], color="green", marker="o", markevery=[-1])
-
-        # plot gyro per x, y, z
-        accPlotAllowed = [1, 1, 1]
-        # plot acc per x, y, z
-        gyroPlotAllowed = [1, 1, 1]
 
         accPlots = []
         gyroPlots = []
 
-        for i, createLine in enumerate(accPlotAllowed):
-            if createLine == 1:
-                accPlots.append(accAxes.plot(timeValues, accValues[i], label=accLegends[i])[0])
-
-        for i, createLine in enumerate(gyroPlotAllowed):
-            if createLine == 1:
-                gyroPlots.append(gyroAxes.plot(timeValues, gyroValues[i], label=gyroLegends[i])[0])
+        # accAxes.plot(self.dataX, self.dataY[0], label=accLegends[0])
+        # accAxes.plot(self.dataX, self.dataY[1], label=accLegends[1])
+        accAxes.plot(self.dataX, self.dataY[2], label=accLegends[2])
+        # gyroAxes.plot(self.dataX, self.dataY[0], label=gyroLegends[0])
+        # gyroAxes.plot(self.dataX, self.dataY[1], label=gyroLegends[1])
+        gyroAxes.plot(self.dataX, self.dataY[2], label=gyroLegends[2])
 
         accGyroPlotsList = [accPlots, gyroPlots]
 
         accAxes.legend(loc='upper right', fancybox=True, shadow=True)
         gyroAxes.legend(loc='upper right', fancybox=True, shadow=True)
 
-        grafico = FigureCanvasTkAgg(fig, topLevel)
-        # grafico.draw()
-        #
-        grafico.get_tk_widget().place(relx=0.05, rely=0.0, relwidth=0.9, relheight=0.5)
-        grafico3d.get_tk_widget().place(relx=0.05, rely=0.5, relwidth=0.9, relheight=0.5)
+        grafico2d.get_tk_widget().place(relx=0.05, rely=0.0, relwidth=0.9, relheight=0.5)
+        grafico3d.get_tk_widget().place(relx=0.05, rely=0.5, relwidth=0.9, relheight=0.4)
 
         # imgGrafico3d = tk.Button(topLevel)
         # imgGrafico3d.place(relx=0.05, rely=0.15)
@@ -199,7 +142,7 @@ class Gui():
         # triangoloZLabel.configure(borderwidth=0)
         #
         # # ----------------------------------------------------------------------
-        #
+
         # codIntervento = tk.Label(topLevel)
         # codIntervento.place(relx=0.6, rely=0.01, )
         # codIntervento.configure(background="#d9d9d9")
@@ -253,55 +196,85 @@ class Gui():
         #     img2.configure(image=imgResized, borderwidth=0)
         #     img2.image = imgResized
         #
-        # avviaButton = tk.Button(topLevel)
-        # avviaButton.config(text=labelFromFile['buttonavviaintervento'], command=lambda: actionAvviaButton())
-        # avviaButton.place(relx=0.85, rely=0.6, height=50, width=150, bordermode='ignore')
-        #
-        # fermaButton = tk.Button(topLevel)
-        # fermaButton.config(text=labelFromFile['buttonsospendiintervento'], command=lambda: actionFermaButton())
-        # fermaButton.place(relx=0.85, rely=0.7, height=50, width=150, bordermode='ignore')
-        #
-        # chiudiFinestraButton = tk.Button(topLevel)
-        # chiudiFinestraButton.config(text=labelFromFile['buttonteminaintervento'], command=lambda: actionchiudiButton())
-        # chiudiFinestraButton.place(relx=0.85, rely=0.8, height=50, width=150, bordermode='ignore')
-        #
-        # fig3d, ax3d = plt.subplots(subplot_kw={'projection': '3d'})
-        # ax3d.set_xticklabels([])
-        # ax3d.set_yticklabels([])
-        # ax3d.set_zticklabels([])
-        #
-        # plt.gcf().canvas.set_window_title('Chirurgia Guidata Os')
-        #
-        # def creaPlotter3d():
-        #     plt.show()
-        #
-        # def actionAvviaButton():
-        #     if avviaButton["text"] == labelFromFile['buttonavviaintervento']:
-        #         avviaButton["text"] = labelFromFile['buttonriprendiintervento']
-        #     else:
-        #         self.calibrazioneSensore()
-        #         # showinfo("Chirurgia Guidata OS", labelFromFile["messaggioconfermacalibrazione"])
-        #     self.playGrafico = True
-        #     avviaButton.configure(state="disable")
-        #     fermaButton.configure(state="normal")
-        #     chiudiFinestraButton.configure(state="disable")
-        #     self.updateGrafico(accGyroPlotsList, grafico, valueLabelTemp, labelFromFile['labeltitolotemperatura'],
-        #                        [progressX, progressY, progressZ], [triangoloXLabel, triangoloYLabel, triangoloZLabel], ax3d)
-        #     # self.updateGrafico3d(ax3d, grafico3D)
-        #     # self.updateGrafico3d(ax3d2, grafico3D)
-        #     self.updateTimer(valueTimer, labelFromFile['labeltitolotimer'])
-        #
-        # def actionFermaButton():
-        #     self.playGrafico = False
-        #     avviaButton.configure(state="normal")
-        #     fermaButton.configure(state="disable")
-        #     chiudiFinestraButton.configure(state="normal")
-        #
-        # def actionchiudiButton():
-        #     avviaButton.configure(state="normal")
-        #     fermaButton.configure(state="normal")
-        #     plt.close(fig3d)
-        #     self.closeTopLevelPlotter(topLevel)
+        avviaButton = tk.Button(topLevel)
+        # avviaButton.config(text="Avvia Lettura", command=lambda: self.aggiornaGrafici(True, fig, fig3d, accGyroPlotsList, topLevel, grafico2d, grafico3d))
+        avviaButton.config(text="Avvia Lettura", command=lambda: setPlay(True))
+        avviaButton.place(relx=0.05, rely=0.9, relwidth=0.2, relheight=0.1, bordermode='ignore')
+
+        stopButton = tk.Button(topLevel)
+        # stopButton.config(text="Ferma Lettura", command=lambda: self.aggiornaGrafici(False, fig, fig3d, accGyroPlotsList, topLevel, grafico2d, grafico3d))
+        stopButton.config(text="Ferma Lettura", command=lambda: setPlay(False))
+        stopButton.place(relx=0.26, rely=0.9, relwidth=0.2, relheight=0.1, bordermode='ignore')
+
+        def disabilitaRotazione(event):
+            ax3d.view_init(elev=45, azim=45)
+
+        fig3d.canvas.mpl_connect('motion_notify_event', disabilitaRotazione)
+
+        def setPlay(val):
+            if val:
+                self.play = val
+                self.aggiornaGrafici(fig, fig3d, accGyroPlotsList, topLevel, grafico2d, grafico3d)
+            else:
+                self.play = val
+                # stop = val
+
+    def aggiornaGrafici(self, figure2d, figure3d, accGyroPlotsList, topLevel, grafico2d, grafico3d):
+
+        startTime = time.time()
+        axes2dAcc = figure2d.axes[0]
+        axes2dGyro = figure2d.axes[1]
+        axes3dAcc = figure3d.axes[0]
+
+        if self.play:
+
+            x = round(random.uniform(-1, 1), 1)
+            y = round(random.uniform(-1, 1), 1)
+            z = round(random.uniform(-1, 1), 1)
+
+            del self.dataY[0][0]
+            del self.dataY[1][0]
+            del self.dataY[2][0]
+
+            self.dataY[0].append(x)
+            self.dataY[1].append(y)
+            self.dataY[2].append(z)
+
+            axes2dAcc.clear()
+            axes2dAcc.plot(self.dataX, self.dataY[0])
+            axes2dAcc.plot(self.dataX, self.dataY[1])
+            axes2dAcc.plot(self.dataX, self.dataY[2])
+            grafico2d.draw()
+
+            axes2dGyro.clear()
+            axes2dGyro.plot(self.dataX, self.dataY[0])
+            axes2dGyro.plot(self.dataX, self.dataY[1])
+            axes2dGyro.plot(self.dataX, self.dataY[2])
+            grafico2d.draw()
+
+            axes3dAcc.clear()
+            axes3dAcc.set_xticklabels([])
+            axes3dAcc.set_yticklabels([])
+            axes3dAcc.set_zticklabels([])
+            axes3dAcc.set_xlim(-2, 2)
+            axes3dAcc.set_ylim(-2, 2)
+            axes3dAcc.set_zlim(-2, 2)
+            axes3dAcc.plot([0, x], [0, 0], [0, 0], color="red", marker="o", markevery=[-1])
+            axes3dAcc.plot([0, 0], [0, y
+
+
+
+
+
+                                    ], [0, 0], color="blue", marker="o", markevery=[-1])
+            axes3dAcc.plot([0, 0], [0, 0], [0, z], color="green", marker="o", markevery=[-1])
+            grafico3d.draw()
+
+            nowTime = time.time()
+            print(round(nowTime - startTime, 3), "seconds")
+
+            topLevel.update()
+            topLevel.after(1, self.aggiornaGrafici(figure2d, figure3d, accGyroPlotsList, topLevel, grafico2d, grafico3d))
 
 
 if __name__ == '__main__':
